c0a1706e602cf54f93cc60523dd2929f43722c04
Priscilla <mycilla74@gmail.com>
2017-10-26 17:07:24 +0100

    got website to get database of questions.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
 	console.log("The quiz has been rendered.")
-	let quiz = (() => {
-		getQuiz = (() => {
+	let quiz = {
+		getQuiz : (() => {
 			console.log("Trying to get the quiz.");
-			$getJSON("https://opentdb.com/api.php?amount=15&category=15&type=multiple", (data) => {
+			$.getJSON("https://opentdb.com/api.php?amount=15&category=15&type=multiple", (data) => {
 				console.log("Got the quiz!");
 				console.log(data)
 			})
 		})
-	})
+	}
+	quiz.getQuiz()
 })
-quiz()
