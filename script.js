0e340337f1b54a923841b592c1ba76039efe4e30
Priscilla <mycilla74@gmail.com>
2017-10-26 16:50:40 +0100

    restart the code.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $(document).ready(() => {
-	let getQuiz = () => {
-		console.log("Getting quiz..")
-		$.getJSON("https://opentdb.com/api.php?amount=15&category=15&type=multiple", (data) => {
-		console.log("Got quiz")
-		console.log(data)
-	}
+	console.log("The quiz has been rendered.")
+	let quiz = (() => {
+		getQuiz = (() => {
+			console.log("Trying to get the quiz.");
+			$getJSON("https://opentdb.com/api.php?amount=15&category=15&type=multiple", (data) => {
+				console.log("Got the quiz!");
+				console.log(data)
+			})
+		})
+	})
 })
+quiz()
