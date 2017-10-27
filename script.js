cb4243f9ffe1910f1abf059de51f722ba097d217
Priscilla <mycilla74@gmail.com>
2017-10-27 10:30:01 +0100

    add code for random question to appear.

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ $(document).ready(() => {
 			$.getJSON("https://opentdb.com/api.php?amount=15&category=15&type=multiple", (data) => {
 				console.log("Got the quiz!");
 				console.log(data)
+
+				let getQuestion = (question) => {
+					console.log(question);
+					$("#questions").html(question.question)
+					let correctChoice = Math.floor(Math.random() * 4);
+					console.log(correctChoice + " was picked");
+					for (i = 0; i < 4; i++) {
+						if (correctChoice === i) {
+							$("#choice" + i).html(questions.correct_answer);
+						} else {
+							$("#choice" + i).html(questions.incorrect_answer);
+						}
+					}
+				}
+				let currentQuestion = 0;
+				getQuestion(data.results[currentQuestion]);
+				$("#nextQuestion").hide();
+				$("#finalScore").html("0 / 10")
 			})
 		})
 	}
